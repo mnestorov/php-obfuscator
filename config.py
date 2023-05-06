a4681941c57b87a5b38010ec358c22d05b725bc9
Martin Nestorov <mbnestorov@gmail.com>
2023-05-06 22:45:28 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configure colors for printing messages
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
+# Log filename
+log_filename = 'app.log'
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /home/YOUR_USERNAME/path/to/your/script/main.py
