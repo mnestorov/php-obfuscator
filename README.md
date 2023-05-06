e9f84087d5db91ef78830a18ad167e47613aeb21
Martin Nestorov <mbnestorov@gmail.com>
2023-05-07 01:47:22 +0300

    Updated README.md, code logic and performance

---
+++ README.md
@@ -0,0 +1,119 @@
+# PHP Obfuscator
+
+## Overview
+
+PHP Obfuscator is a command-line tool to obfuscate PHP source code files using YAK Pro - Php Obfuscator and PHP-Parser. The tool aims to protect the intellectual property of your PHP project by making it more difficult for others to understand or reverse-engineer your code. The tool can be used to obfuscate single files, multiple files, or an entire project directory.
+
+## Requirements
+
+- Python 3.6+
+- PHP-Parser (PHP library)
+- YAK Pro - Php Obfuscator (PHP library)
+
+## Installation
+
+1. Clone the repository
+
+```
+git clone https://github.com/your-github-repo/php-obfuscator.git
+```
+
+2. Change to the project directory
+
+```
+cd php-obfuscator
+```
+
+3. Install YAK Pro - Php Obfuscator
+
+You need to install this in to the project directory **_php-obfuscator_**
+
+```
+git clone https://github.com/pk-fr/yakpro-po.git
+```
+
+After that, you need to configure the package from `yakpro-po.cnf` in to the **_php-obfuscator > yakpro-po_** folder.
+
+**Recommended settings for the YAK Pro configuration:**
+
+```php
+// Allowed modes are: 'PREFER_PHP7', 'PREFER_PHP5', 'ONLY_PHP7', 'ONLY_PHP5'
+$conf->parser_mode = 'PREFER_PHP7'; 
+
+$conf->obfuscate_constant_name = false;         
+$conf->obfuscate_variable_name = false;        
+$conf->obfuscate_function_name = false;        
+$conf->obfuscate_class_name = false;         
+$conf->obfuscate_interface_name = false;         
+$conf->obfuscate_trait_name = false;         
+$conf->obfuscate_class_constant_name = false;        
+$conf->obfuscate_property_name = false;        
+$conf->obfuscate_method_name = false;         
+$conf->obfuscate_namespace_name = false;         
+$conf->obfuscate_label_name = false;    
+
+// User comment to insert inside each obfuscated file
+$conf->user_comment = false;         
+```     
+
+4. Install PHP-Parser
+
+You need to install this in to the **yakpro-po** directory in to the project directory **_php-obfuscator > yakpro-po_**
+
+```
+git clone https://github.com/nikic/PHP-Parser.git
+```
+5. Install the required Python packages
+
+**From the project directory, you need to run this command:**
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+1. Ensure you are in the project directory.
+2. Run the script
+
+```
+python main.py
+```
+
+or
+
+```
+bash start.sh
+```
+
+**Note:** Don't forget to change the path to the script in to the `start.sh` bash file.
+
+3. Follow the prompts to configure the obfuscation settings, including:
+    - Mode (single file, multiple files, or entire project directory)
+    - Output directory path
+    - File or directory paths to exclude
+    - Whether to create backups of original PHP files
+
+4. After the obfuscation process is completed, you can find the obfuscated files in the specified output directory.
+
+## Used Libraries
+
+### Python
+
+- **os**, **sys**, **shutil**, and **re** - standard Python libraries for working with the file system and regular expressions
+- **logging** - standard Python library for logging
+- **concurrent.futures** - standard Python library for parallel processing
+- **tqdm** - external library for progress bars
+
+### PHP
+
+- [YAK Pro - a PHP library for obfuscating PHP code](https://github.com/pk-fr/yakpro-po)
+- [PHP-Parser - a PHP library to parse and traverse PHP code](https://github.com/nikic/PHP-Parser/tree/4.x/)
+
+## Troubleshooting
+
+If you encounter issues after obfuscating your PHP project, you may need to revert your files to the original (non-obfuscated) versions and reevaluate your obfuscation strategy. Always keep backups of your original code before applying obfuscation, as it can be difficult or impossible to reverse the process and recover the original code.
+
+## License
+
+This project is released under the MIT License.
--- main.py
+++ main.py
@@ -1,51 +1,26 @@
 import sys
-import base64
 import os
-import re
 import logging
 import shutil
-from zlib import compress
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 from config import (log_filename, YELLOW, BLUE, GREEN, RED, RESET)
+from subprocess import call
 
 # Configure logging
 logging.basicConfig(filename=log_filename, level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-def obfuscate_php(input_file, obfuscation_level, create_backup, output_directory):
+def obfuscate_php(input_file, create_backup, output_directory):
     if create_backup:
         backup_file = f"{os.path.splitext(input_file)[0]}_backup.php"
         shutil.copy2(input_file, backup_file)
         logging.info(f"Created backup: {backup_file}")
 
     try:
-        logging.info(f"Obfuscating {input_file} with level: {obfuscation_level}")
-
-        with open(input_file, "r") as f:
-            php_code = f.read()
-
-        if obfuscation_level == 'low':
-            encoded_php_code = base64.b64encode(php_code.encode()).decode()
-            obfuscated_code = f"<?php eval(base64_decode('{encoded_php_code}')); ?>"
-        elif obfuscation_level == 'medium':
-            compressed_php_code = compress(php_code.encode())
-            encoded_php_code = base64.b64encode(compressed_php_code).decode()
-            obfuscated_code = f"<?php eval(gzinflate(base64_decode('{encoded_php_code}'))); ?>"
-        elif obfuscation_level == 'high':
-            compressed_php_code = compress(php_code.encode())
-            encoded_php_code = base64.b64encode(compressed_php_code).decode()
-            obfuscated_code = f"<?php eval(gzinflate(base64_decode('{encoded_php_code}'))); ?>"
-
-            var_pattern = re.compile(r'\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*')
-            var_names = set(var_pattern.findall(obfuscated_code))
-            for var_name in var_names:
-                new_var_name = f"${obfuscation_level}_{var_name[1:]}"
-                obfuscated_code = obfuscated_code.replace(var_name, new_var_name)
-
-        # Save the obfuscated code to the output directory
         output_file = os.path.join(output_directory, f"obfuscated_{os.path.basename(input_file)}")
-        with open(output_file, "w") as f:
-            f.write(obfuscated_code)
+        logging.info(f"Obfuscating {input_file}")
+
+        call(["/home/mnestorov/AutomationScripts/ObfuscatesPhpCode/yakpro-po/yakpro-po.php", "-o", output_file, input_file])
 
         print(f"{GREEN}Obfuscated file saved as {output_file}{RESET}")
         logging.info(f"Obfuscated {input_file} successfully")
@@ -55,10 +30,10 @@ def obfuscate_php(input_file, obfuscation_level, create_backup, output_directory
         print(f"{RED}Error while obfuscating {input_file}: {e}{RESET}")
 
 def obfuscate_file(args):
-    input_file, obfuscation_level, create_backup, output_directory = args
-    obfuscate_php(input_file, obfuscation_level, create_backup, output_directory)
+    input_file, create_backup, output_directory = args
+    obfuscate_php(input_file, create_backup, output_directory)
 
-def process_directory(directory, obfuscation_level, exclude_list, create_backup, output_directory, max_workers=4):
+def process_directory(directory, exclude_list, create_backup, output_directory, max_workers=4):
     total_files = 0
     for root, _, files in os.walk(directory):
         for file in files:
@@ -77,7 +52,7 @@ def process_directory(directory, obfuscation_level, exclude_list, create_backup,
                     logging.info(f"Skipping {input_file}: excluded")
                     continue
 
-                file_list.append((input_file, obfuscation_level, create_backup, output_directory))
+                file_list.append((input_file, create_backup, output_directory))
 
     progress_bar = tqdm(total=len(file_list), desc="Obfuscating", unit="file")
 
@@ -86,9 +61,9 @@ def process_directory(directory, obfuscation_level, exclude_list, create_backup,
             progress_bar.update()
 
     progress_bar.close()
-
-def interactive_mode():
-    print(f"{GREEN}Welcome to the PHP Obfuscator Interactive Mode!{RESET}")
+        
+def main():
+    print(f"{GREEN}Welcome to the PHP Obfuscator!{RESET}")
     print(f"{GREEN}Follow the prompts to obfuscate your PHP files.\n{RESET}")
 
     print(f"{GREEN}Choose the mode for obfuscating your PHP files:{RESET}")
@@ -97,102 +72,47 @@ def interactive_mode():
     print(f"{BLUE}3: Entire project directory{RESET}")
     mode = input(f"{GREEN}Enter the mode number (1/2/3): {RESET}")
 
-    print(f"{GREEN}\nChoose the obfuscation level:{RESET}")
-    print(f"{BLUE}1: Low (Base64 encoding){RESET}")
-    print(f"{BLUE}2: Medium (zlib compression + Base64 encoding){RESET}")
-    print(f"{BLUE}3: High (zlib compression + Base64 encoding + simple variable renaming){RESET}")
-    obfuscation_level = input(f"{GREEN}Enter the obfuscation level number (1/2/3): {RESET}")
-
-    exclude_input = input(f"{GREEN}\nEnter a comma-separated list of files or directories to exclude (leave empty if none): {RESET}")
-    exclude_list = [path.strip() for path in exclude_input.split(',') if path.strip()]
-
-    backup_input = input(f"{GREEN}\nCreate backups of original PHP files? (y/n): {RESET}")
-    create_backup = backup_input.lower() == 'y'
-
-    output_directory = input(f"{GREEN}\nEnter the output directory path: {RESET}")
+    output_directory = input(f"{GREEN}Enter the output directory path: {RESET}")
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
+    exclude_input = input(f"{GREEN}Enter file or directory paths to exclude (separated by a space) (you can skip this step): {RESET}")
+    exclude_list = [os.path.abspath(exclude.strip()) for exclude in exclude_input.split()]
+
+    backup_input = input(f"{GREEN}Create backups of original PHP files? (y/n): {RESET}").lower()
+    create_backup = backup_input == 'y'
+
     if mode == '1':
-        input_file = input(f"{GREEN}\nEnter the PHP file path: {RESET}")
-        obfuscate_php(input_file, obfuscation_level, create_backup, output_directory)
-    elif mode == '2':
-        input_files = input(f"{GREEN}\nEnter a comma-separated list of PHP files to obfuscate: {RESET}")
-        files = [file.strip() for file in input_files.split(',') if file.strip()]
-        for file in files:
-            obfuscate_php(file, obfuscation_level, create_backup, output_directory)
-    elif mode == '3':
-        input_directory = input(f"{GREEN}\nEnter the directory path containing PHP files: {RESET}")
-        process_directory(input_directory, obfuscation_level, exclude_list, create_backup, output_directory)
-    else:
-        print(f"{RED}Invalid mode. Exiting...{RESET}")
-        sys.exit(1)
-        
-def main():
-    interactive = input(f"{YELLOW}Do you want to run the script in interactive mode? (y/n): {RESET}").lower()
-    if interactive == 'y':
-        interactive_mode()
-    else:
-        print(f"{GREEN}Choose the mode for obfuscating your PHP files: {RESET}")
-        print(f"{BLUE}1: Single file{RESET}")
-        print(f"{BLUE}2: Multiple files{RESET}")
-        print(f"{BLUE}3: Entire project directory{RESET}")
-        mode = input(f"{GREEN}Enter the mode number (1/2/3): {RESET}")
-
-        print(f"{GREEN}\nSelect obfuscation level: {RESET}")
-        print(f"{BLUE}1: Low (Base64 encoding){RESET}")
-        print(f"{BLUE}2: Medium (zlib compression + Base64 encoding){RESET}")
-        print(f"{BLUE}3: High (zlib compression + Base64 encoding + simple variable renaming){RESET}")
-        obfuscation_level = input(f"{GREEN}Enter the obfuscation level number (1/2/3): {RESET}")
-
-        output_directory = input(f"{GREEN}Enter the output directory path: {RESET}")
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-
-        level_mapping = {'1': 'low', '2': 'medium', '3': 'high'}
-        if obfuscation_level not in level_mapping:
-            logging.warning("Invalid obfuscation level. Choose from: 1, 2, or 3")
-            print(f"{RED}Invalid obfuscation level. Choose from: 1, 2, or 3{RESET}")
+        input_file = input(f"{GREEN}Enter the PHP file path: {RESET}")
+        if not input_file.lower().endswith(".php") or not os.path.isfile(input_file):
+            logging.warning("Invalid PHP file path")
+            print(f"{RED}Invalid PHP file path{RESET}")
             sys.exit(1)
-        obfuscation_level = level_mapping[obfuscation_level]
-
-        exclude_input = input(f"{GREEN}Enter file or directory paths to exclude (separated by a space): {RESET}")
-        exclude_list = [os.path.abspath(exclude.strip()) for exclude in exclude_input.split()]
-
-        backup_input = input(f"{GREEN}Create backups of original PHP files? (y/n): {RESET}").lower()
-        create_backup = backup_input == 'y'
-
-        if mode == '1':
-            input_file = input(f"{GREEN}Enter the PHP file path: {RESET}")
-            if not input_file.lower().endswith(".php") or not os.path.isfile(input_file):
-                logging.warning("Invalid PHP file path")
-                print(f"{RED}Invalid PHP file path{RESET}")
-                sys.exit(1)
-            if any(os.path.commonpath([input_file, exclude]) == os.path.abspath(exclude) for exclude in exclude_list):
-                logging.info(f"Skipping {input_file}: excluded")
-            else:
-                obfuscate_php(input_file, obfuscation_level, create_backup)
-        elif mode == '2':
-            file_paths = input(f"{GREEN}Enter the PHP file paths separated by a space: {RESET}")
-            files = file_paths.split()
-
-            for input_file in files:
-                if not input_file.lower().endswith(".php") or not os.path.isfile(input_file):
-                    logging.warning(f"Skipping {input_file}: not a valid PHP file")
-                    print(f"{RED}Skipping {input_file}: not a valid PHP file{RESET}")
-                    continue
-                obfuscate_php(input_file, obfuscation_level, create_backup)
-        elif mode == '3':
-            input_directory = input(f"{GREEN}Enter the project directory path: {RESET}")
-            if not os.path.isdir(input_directory):
-                logging.warning("Invalid directory path")
-                print(f"{RED}Invalid directory path{RESET}")
-                sys.exit(1)
-            process_directory(input_directory, obfuscation_level, exclude_list, create_backup)
+        if any(os.path.commonpath([input_file, exclude]) == os.path.abspath(exclude) for exclude in exclude_list):
+            logging.info(f"Skipping {input_file}: excluded")
         else:
-            logging.warning("Invalid mode. Choose from: 1, 2, or 3")
-            print(f"{RED}Invalid mode. Choose from: 1, 2, or 3{RESET}")
+            obfuscate_php(input_file, create_backup, output_directory)
+    elif mode == '2':
+        file_paths = input(f"{GREEN}Enter the PHP file paths separated by a space: {RESET}")
+        files = file_paths.split()
+
+        for input_file in files:
+            if not input_file.lower().endswith(".php") or not os.path.isfile(input_file):
+                logging.warning(f"Skipping {input_file}: not a valid PHP file")
+                print(f"{RED}Skipping {input_file}: not a valid PHP file{RESET}")
+                continue
+            obfuscate_php(input_file, create_backup, output_directory)
+    elif mode == '3':
+        input_directory = input(f"{GREEN}Enter the project directory path: {RESET}")
+        if not os.path.isdir(input_directory):
+            logging.warning("Invalid directory path")
+            print(f"{RED}Invalid directory path{RESET}")
             sys.exit(1)
+        process_directory(input_directory, exclude_list, create_backup, output_directory)
+    else:
+        logging.warning("Invalid mode. Choose from: 1, 2, or 3")
+        print(f"{RED}Invalid mode. Choose from: 1, 2, or 3{RESET}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
