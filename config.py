e3c1f10dfb5c05f02e97ba64654c0de337747074
Martin Nestorov <mbnestorov@gmail.com>
2023-05-07 02:26:23 +0300

    Path correction for the Yak Pro package on config file

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ RED = '\033[91m'
 RESET = '\033[0m'
 
 # Configure YakPro packages path
-YAKPRO = ["/home/mnestorov/AutomationScripts/ObfuscatesPhpCode/yakpro-po/yakpro-po.php", "-o"]
+YAKPRO = ["/home/YOUR_USERNAME/PROJECT_DIR/yakpro-po/yakpro-po.php", "-o"]
 
 # Log filename
 log_filename = 'app.log'
